8952b75910505d9b92370e64f909d157c66c581f
Tim Destan <tim.destan@gmail.com>
2019-11-16 15:12:10 -0500

    Script to sync files to home directory.

---
+++ sync.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copies selected files from the repository to the current user's home
+# directory.
+
+set -o errexit
+set -o nounset
+
+while (( "$#" )); do
+  case "$1" in
+    -v|--verbose)
+      set -x
+      shift
+      ;;
+    *) # unsupported flags or extra arguments.
+      echo "Error: Bad arg $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+FILES=(
+  .gitconfig
+  .tmux.conf
+  .cargo/config
+)
+
+for f in ${FILES[*]} ; do
+  dir=$(dirname $f)
+  if [[ $dir != "." ]] ; then
+    mkdir -p "~/$dir"
+  fi
+  cp $f ~/$f
+done
