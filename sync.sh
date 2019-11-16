5b2f962cd080d828c4fc7c7d109a088939f306ee
Tim Destan <tim.destan@gmail.com>
2019-11-16 15:45:23 -0500

    Support reverse sync (from home dir to git repo).

--- sync.sh
+++ sync.sh
@@ -2,14 +2,24 @@
 #
 # Copies selected files from the repository to the current user's home
 # directory.
+#
+# Options:
+# --verbose | -v : Echo commands.
+# --reverse | -r : Instead copy files from home directory to the repo.
 
 set -o errexit
 set -o nounset
+
+REV=0
 
 while (( "$#" )); do
   case "$1" in
     -v|--verbose)
       set -o xtrace
+      shift
+      ;;
+    -r|--reverse)
+      REV=1
       shift
       ;;
     *) # unsupported flags or extra arguments.
@@ -26,9 +36,17 @@ FILES=(
 )
 
 for f in ${FILES[*]} ; do
-  dir=$(dirname $f)
-  if [[ $dir != "." ]] ; then
-    mkdir -p "~/$dir"
+  if (( $REV )) ; then
+    if test -f ~/$f ; then
+      cp ~/$f $f
+    else
+      echo "~/$f doesn't exist." >&2
+    fi
+  else
+    dir=$(dirname $f)
+    if [[ $dir != "." ]] ; then
+      mkdir -p "~/$dir"
+    fi
+    cp $f ~/$f
   fi
-  cp $f ~/$f
 done
